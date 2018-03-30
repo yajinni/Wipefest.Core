273c977fe69740baf5aa6356fe9c7f6e0d8baf41
JoshYaxley <joshyaxley93@gmail.com>
2018-03-30 16:41:10 +0100

    Run Chrome without sandbox

--- karma.conf.js
+++ karma.conf.js
@@ -1,45 +1,56 @@
-var webpackConfig = require("./webpack.config.js");
+var webpackConfig = require('./webpack.config.js');
 
 module.exports = function (config) {
-    config.set({
-        // Find all files that match this glob
-        files: [
-            "**/*.spec.ts"
-        ],
-        // Before running any tests, run them through webpack
-        preprocessors: {
-            '**/*.spec.ts': ["webpack"]
-        },
-        // Use the following webpack configuration (pulled from webpack.config.js)
-        // webpack.entry will be set to the file currently being preprocessed
-        webpack: {
-            mode: webpackConfig.mode,
-            devtool: webpackConfig.devtool,
-            resolve: webpackConfig.resolve,
-            module: webpackConfig.module
-        },
-        // Allows karma to read ts files
-        mime: {
-            'text/x-typescript': ["ts", "tsx"]
-        },
-        
-        browsers: ["Chrome"],
-        reporters: ["progress", "kjhtml"],
-        coverageIstanbulReporter: {
-            reports: ["html", "lcovonly"],
-            fixWebpackSourcePaths: true
-        },
-        client: {
-            clearContext: false // leave Jasmine Spec Runner output visible in browser
-        },
+  const configuration = {
+    // Find all files that match this glob
+    files: ['**/*.spec.ts'],
+    // Before running any tests, run them through webpack
+    preprocessors: {
+      "**/*.spec.ts": ['webpack']
+    },
+    // Use the following webpack configuration (pulled from webpack.config.js)
+    // webpack.entry will be set to the file currently being preprocessed
+    webpack: {
+      mode: webpackConfig.mode,
+      devtool: webpackConfig.devtool,
+      resolve: webpackConfig.resolve,
+      module: webpackConfig.module
+    },
+    // Allows karma to read ts files
+    mime: {
+      "text/x-typescript": ['ts', 'tsx']
+    },
 
-        basePath: "",
-        frameworks: ["jasmine"],
-        port: 9876,
-        colors: true,
-        logLevel: config.LOG_INFO,
-        autoWatch: true,
-        singleRun: false,
-        concurrency: Infinity
-    });
-}
+    browsers: ['Chrome'],
+    reporters: ['progress', 'kjhtml'],
+    coverageIstanbulReporter: {
+      reports: ['html', 'lcovonly'],
+      fixWebpackSourcePaths: true
+    },
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+
+    basePath: '',
+    frameworks: ['jasmine'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    singleRun: false,
+    concurrency: Infinity
+  };
+
+  if (process.env.TRAVIS) {
+    // Travis CI Chrome needs to run without sandbox
+    configuration.customLaunchers = {
+      ChromeTravis: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    };
+    configuration.browsers = ['ChromeTravis'];
+  }
+
+  config.set(configuration);
+};
